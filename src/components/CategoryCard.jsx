7cfcf87ba8008dd5615df311cbdc1e7502609127
vickyjha23 <vickyjha388@gmail.com>
2025-01-10 22:28:10 +0530

    hotspotProduct added

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom'
 const CategoryCard = ({ product }) => {
   return (
     <div className='flex flex-col justify-center items-center gap-4 overflow-hidden'>
-          <div className='max-w-[450px] max-h-[300px] flex justify-center items-start before:transition-all before:duration-200 before:ease-linear relative z-[1] before:absolute before:content-[""] before:w-full before:h-full before:bg-[#f9f9f9] before:top-1/2 before:left-1/2 before:-translate-x-1/2 before:-translate-y-1/2 before:-z-[1] before:rounded-[25px] h-[289px] shadow before:border-2 before:border-transparent before:hover:border-yellow-700 rounded-[25px] overflow-hidden'>
+          <div className='w-full max-w-500px lg:max-w-[450px] lg:max-h-[300px] flex justify-center items-start before:transition-all before:duration-200 before:ease-linear relative z-[1] before:absolute before:content-[""] before:w-full before:h-full before:bg-[#f9f9f9] before:top-1/2 before:left-1/2 before:-translate-x-1/2 before:-translate-y-1/2 before:-z-[1] before:rounded-[25px] h-[289px] shadow before:border-2 before:border-transparent before:hover:border-yellow-700 rounded-[25px] overflow-hidden'>
              <Link to='/' className='h-full overflow-hidden flex items-center justify-center'>
-                 <img src={product.image} className='w-[90%] object-cover' alt="" />
+                 <img src={product.image} className='w-full h-full' alt="" />
              </Link>
 
           </div>
--- src/components/MySwiper.jsx
+++ src/components/MySwiper.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faArrowLeft, faArrowRight} from "@fortawesome/free-solid-svg-icons"
+import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons"
 import { Navigation } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
@@ -15,38 +15,40 @@ import CategoryCard from './CategoryCard';
 const MySwiper = () => {
   return (
     <div className='relative'>
-         <Swiper modules={[ Navigation ]} spaceBetween={30} navigation = {{
-         nextEl: ".swiper-next",
-         prevEl: ".swiper-prev",
-    }} onSwiper={(swiper) => console.log(swiper)}
-    onSlideChange={() => console.log('slide change')} className=' w-full !pt-5 select-none' breakpoints={{
+      <Swiper modules={[Navigation]} spaceBetween={30} navigation={{
+        nextEl: ".swiper-next",
+        prevEl: ".swiper-prev",
+      }} onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log('slide change')} className=' w-full !pt-5 select-none' breakpoints={{
           640: {
-              slidesPerView: 2,
-              spaceBetween: 20  
+            slidesPerView: 2,
+            spaceBetween: 20
           },
           768: {
-              slidesPerView: 3,
-              spaceBetween: 20
+            slidesPerView: 3,
+            spaceBetween: 20
           },
           1024: {
-              slidesPerView: 4,
-              spaceBetween: 20
+            slidesPerView: 4,
+            spaceBetween: 20
           },
           1280: {
             slidesPerView: 6,
             spaceBetween: 30
           }
-    }}  >
-         {products.map((product, index) => <SwiperSlide className='w-full' key={index}>
-            <CategoryCard product = {product} />
-         </SwiperSlide>)}   
-    </Swiper>
-         <div className='swiper-prev cursor-pointer flex justify-center items-center transition-all duration-200 ease-linear hover:text-orange-500 hover:border-orange-500'>
-            <FontAwesomeIcon icon={faArrowLeft} className='text-4xl rotate-45' />
-         </div>
-         <div className='swiper-next cursor-pointer flex justify-center items-center transition-all duration-200 ease-linear hover:text-orange-500 hover:border-orange-500'>
-            <FontAwesomeIcon icon={faArrowRight} className='text-4xl rotate-45' />
-         </div>   
+        }}  >
+        {products.map((product, index) => <SwiperSlide className='w-full' key={index}>
+          <CategoryCard product={product} />
+        </SwiperSlide>)}
+      </Swiper>
+      <div className='w-full flex gap-10 justify-center items-center mt-8 md:mt-0 absolute -bottom-20 md:bottom-[450px] md:justify-end'>
+        <div className='swiper-prev cursor-pointer flex justify-center items-center transition-all duration-200 ease-linear hover:text-orange-500 hover:border-orange-500'>
+          <FontAwesomeIcon icon={faArrowLeft} className='text-3xl rotate-45' />
+        </div>
+        <div className='swiper-next cursor-pointer flex justify-center items-center transition-all duration-200 ease-linear hover:text-orange-500 hover:border-orange-500'>
+          <FontAwesomeIcon icon={faArrowRight} className='text-3xl rotate-45' />
+        </div>
+      </div>
     </div>
   )
 }
